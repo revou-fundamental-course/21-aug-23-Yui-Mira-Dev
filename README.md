27dc3847844218f8234b275dcc8d84533d34b082
Yui <adeilhamw2004@gmail.com>
2023-08-31 22:04:53 +0700

    fix?

---
+++ README.md
@@ -0,0 +1,6 @@
+# Hi, Saya Muhammad Ade Ilham Wahyudi
+
+- ### Mini Projek RevoU [Portfolio_Web](https://revou-fundamental-course.github.io/21-aug-23-Yui-Mira-Dev/)
+
+## About Me
+Saya adalah seorang yang tengah merintis perjalanan di dunia pengembangan web. Saya percaya bahwa setiap baris kode adalah peluang untuk belajar dan tumbuh. Saat ini, saya sedang mengasah keterampilan di Politeknik Negeri Sriwijaya. Mari kita bersama-sama menjelajahi dunia web!
--- js/index.js
+++ js/index.js
@@ -1,14 +1,62 @@
+// Get name
 // const name = prompt('Enter your name:');
-// const imageURL = prompt('Enter the image URL Untuk Foto Profile:');
-
 // const nameElement = document.getElementById('name');
-// const imageElement = document.getElementById('userImage');
-
 // nameElement.textContent = name;
-// imageElement.src = imageURL;
 
-// nav
+// slideshow
+let slideIndex = 0;
+let slideshowTimer;
+let isManualClick = false; // menandai jika ada klik manual
 
+showSlides();
+
+function plusDivs(n) {
+    showSlides(slideIndex += n);
+    resetTimer();
+    isManualClick = true;
+    setTimeout(() => {
+        isManualClick = false;
+    }, 1000); // gk ada manual klik, lanjut slide otomatis
+}
+
+function changeSlide(n) {
+    showSlides(slideIndex = n);
+    resetTimer();
+    isManualClick = true;
+    setTimeout(() => {
+        isManualClick = false;
+    }, 1000); 
+}
+
+function showSlides() {
+    let i;
+    const slides = document.getElementsByClassName("img-slideshow");
+    
+    if (slideIndex >= slides.length) {
+        slideIndex = 0;
+    }
+    if (slideIndex < 0) {
+        slideIndex = slides.length - 1;
+    }
+    
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    
+    slides[slideIndex].style.display = "block";
+    slideIndex++;
+
+    if (!isManualClick) {
+        slideshowTimer = setTimeout(showSlides, 10000); // Mengubah slide setiap 10 detik
+    }
+}
+
+function resetTimer() {
+    clearTimeout(slideshowTimer);
+    slideshowTimer = setTimeout(showSlides, 10000); // Reset timer ketika di klik manual
+}
+
+// nav bar android
 document.querySelector('.menu-toggle').addEventListener('click', function() {
     document.querySelector('.nav-list').classList.toggle('active');
     document.querySelector('.overlay').style.display = 'block';
@@ -21,7 +69,9 @@ document.querySelector('.close-btn').addEventListener('click', function() {
     document.querySelector('.close-btn').style.display = 'none';
 });
 
-// data
+
+
+// MessageFrom JS
 function validateForm() {
     const name = document.forms["message-form"]["full-name"].value;
     const birthDate = document.forms["message-form"]["birth-date"].value;
@@ -32,7 +82,7 @@ function validateForm() {
         alert("Data tidak boleh kosong");
         return false;
     }
-
+    
     setSenderUI(name, birthDate, gender, messages);
 
     return false;
@@ -44,3 +94,38 @@ function setSenderUI(name, birthDate, gender, messages) {
     document.getElementById("sender-gender").innerHTML = gender;
     document.getElementById("sender-messages").innerHTML = messages;
 }
+
+// scroll animasi
+window.addEventListener('scroll', () => {
+    const nav = document.querySelector('.nav');
+    if (window.scrollY > nav.offsetTop) {
+        nav.classList.add('fixed-nav');
+    } else {
+        nav.classList.remove('fixed-nav');
+    }
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+    const navLinks = document.querySelectorAll('.nav-list a');
+
+    const smoothScroll = (target) => {
+        const targetElement = document.querySelector(target);
+        if (targetElement) {
+            window.scrollTo({
+                top: targetElement.offsetTop - 100, 
+                behavior: 'smooth',
+            });
+        }
+    };
+
+    navLinks.forEach(link => {
+        link.addEventListener('click', (e) => {
+            e.preventDefault();
+            const target = e.currentTarget.getAttribute('href');
+
+            setTimeout(() => {
+                smoothScroll(target);
+            }, 300); 
+        });
+    });
+});
